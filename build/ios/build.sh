cb5e3160892904026d0f0996ab97425bd8b720aa
superGekFordJ <fordjiang125@gmail.com>
2026-02-02 04:19:45 +0800

    chore: bump version to 1.3.0-beta.2

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="goaria-v3"
 BUNDLE_ID=""
-VERSION="1.3.0-beta.1"
-BUILD_NUMBER="1.3.0-beta.1"
+VERSION="1.3.0-beta.2"
+BUILD_NUMBER="1.3.0-beta.2"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
