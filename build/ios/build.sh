a01515939bd7a4a172036f0ef341829750cb2eef
superGekFordJ <fordjiang125@gmail.com>
2026-04-27 20:00:58 +0800

    chore: bump version to 1.8.0

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="goaria-v3"
 BUNDLE_ID=""
-VERSION="1.7.1"
-BUILD_NUMBER="1.7.1"
+VERSION="1.8.0"
+BUILD_NUMBER="1.8.0"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
