6f4ef4f4c62902a17fbe09a7f5d826a0c2e18f48
superGekFordJ <fordjiang125@gmail.com>
2026-05-14 16:01:23 +0800

    chore(release): prepare v2.0.0-alpha.2

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -4,8 +4,8 @@ set -e
 # Build configuration
 APP_NAME="goaria-v3"
 BUNDLE_ID=""
-VERSION="2.0.0-alpha.1"
-BUILD_NUMBER="2.0.0-alpha.1"
+VERSION="2.0.0"
+BUILD_NUMBER="2.0.0.2"
 BUILD_DIR="build/ios"
 TARGET="simulator"
 
