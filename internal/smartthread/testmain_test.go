d998cf4d4c74c81321469e544903b2618a394bf8
superGekFordJ <fordjiang125@gmail.com>
2026-07-21 18:35:03 +0800

    test(config): add TestMain to smartthread and tasks packages to prevent disk pollution
    config.Update persists to disk via saveLocked. Without USERPROFILE/HOME redirected to a temp dir, tests calling config.Update would overwrite the user's real ~/.goaria/config.json. Add TestMain files to internal/smartthread and internal/tasks that redirect HOME to a temp dir and seed a default config. Also clean up the temp dir in internal/extension/testmain_test.go before os.Exit.

--- internal/smartthread/testmain_test.go
+++ internal/smartthread/testmain_test.go
@@ -1,4 +1,4 @@
-package extension
+package smartthread
 
 import (
 	"os"
@@ -8,13 +8,13 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	tmp, err := os.MkdirTemp("", "goaria-ext-test-")
+	tmp, err := os.MkdirTemp("", "goaria-smartthread-test-")
 	if err != nil {
 		panic(err)
 	}
 	_ = os.Setenv("USERPROFILE", tmp)
 	_ = os.Setenv("HOME", tmp)
-	config.SetTestConfig(&config.AppConfig{})
+	config.SetTestConfig(&config.AppConfig{MinThreadLife: 5})
 	code := m.Run()
 	_ = os.RemoveAll(tmp)
 	os.Exit(code)
--- internal/tasks/testmain_test.go
+++ internal/tasks/testmain_test.go
@@ -1,4 +1,4 @@
-package extension
+package tasks
 
 import (
 	"os"
@@ -8,13 +8,16 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	tmp, err := os.MkdirTemp("", "goaria-ext-test-")
+	tmp, err := os.MkdirTemp("", "goaria-tasks-test-")
 	if err != nil {
 		panic(err)
 	}
 	_ = os.Setenv("USERPROFILE", tmp)
 	_ = os.Setenv("HOME", tmp)
-	config.SetTestConfig(&config.AppConfig{})
+	config.SetTestConfig(&config.AppConfig{
+		DownloadDir:     os.TempDir(),
+		SmartThreadMode: false,
+	})
 	code := m.Run()
 	_ = os.RemoveAll(tmp)
 	os.Exit(code)
