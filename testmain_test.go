79d4834a8ed851a5c484be0fe0ce514aaddf10f9
superGekFordJ <fordjiang125@gmail.com>
2026-07-23 03:18:08 +0800

    test: add TestMain fixture to set test config for main package

---
+++ testmain_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"goaria-v3/internal/config"
+)
+
+func TestMain(m *testing.M) {
+	config.SetTestConfig(&config.AppConfig{
+		DownloadDir:            os.TempDir(),
+		MaxConnections:         "32",
+		MaxConcurrentDownloads: "3",
+		UserAgent:              "Mozilla/5.0 (test)",
+	})
+	code := m.Run()
+	os.Exit(code)
+}
