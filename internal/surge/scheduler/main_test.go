d8e97ce079c7d0dc896af5077b96033b5fab885e
superGekFordJ <fordjiang125@gmail.com>
2026-08-26 15:09:24 +0800

    test: drain DefaultNetworkPool in scheduler and concurrent TestMain

--- internal/surge/scheduler/main_test.go
+++ internal/surge/scheduler/main_test.go
@@ -2,10 +2,13 @@
 package scheduler
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"testing"
 
 	goariaconfig "goaria-v3/internal/config"
+	"goaria-v3/internal/surge/transport"
 
 	"go.uber.org/goleak"
 )
@@ -17,8 +20,19 @@ func TestMain(m *testing.M) {
 		MaxConcurrentDownloads: "3",
 		UserAgent:              "Mozilla/5.0 (test)",
 	})
-	goleak.VerifyTestMain(m,
-		goleak.IgnoreTopFunction("sync.runtime_notifyListWait"),
-		goleak.IgnoreTopFunction("goaria-v3/internal/surge/scheduler.safeSendProgress"),
-	)
+	code := m.Run()
+	http.DefaultClient.CloseIdleConnections()
+	transport.DefaultNetworkPool.CloseAll()
+	if code == 0 {
+		if err := goleak.Find(
+			goleak.IgnoreTopFunction("sync.runtime_notifyListWait"),
+			goleak.IgnoreTopFunction("goaria-v3/internal/surge/scheduler.safeSendProgress"),
+			goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
+			goleak.IgnoreTopFunction("net/http.(*persistConn).readLoop"),
+		); err != nil {
+			fmt.Fprintf(os.Stderr, "goleak: Errors on successful test run: %v\n", err)
+			os.Exit(1)
+		}
+	}
+	os.Exit(code)
 }
--- internal/surge/strategy/concurrent/main_test.go
+++ internal/surge/strategy/concurrent/main_test.go
@@ -2,11 +2,28 @@
 package concurrent
 
 import (
+	"fmt"
+	"net/http"
+	"os"
 	"testing"
+
+	"goaria-v3/internal/surge/transport"
 
 	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m)
+	code := m.Run()
+	http.DefaultClient.CloseIdleConnections()
+	transport.DefaultNetworkPool.CloseAll()
+	if code == 0 {
+		if err := goleak.Find(
+			goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
+			goleak.IgnoreTopFunction("net/http.(*persistConn).readLoop"),
+		); err != nil {
+			fmt.Fprintf(os.Stderr, "goleak: Errors on successful test run: %v\n", err)
+			os.Exit(1)
+		}
+	}
+	os.Exit(code)
 }
